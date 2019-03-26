9285f251e8a3fe2325865cc72f4d7cdbed7b392f
TissueRoll <christopher.dizon@protonmail.com>
2019-03-26 14:39:29 +0800

    base completed i think

--- src/generators/Driver.java
+++ src/generators/Driver.java
@@ -4,5 +4,6 @@ public class Driver {
 	public static void main(String[] args) throws Exception {
 		new InterfaceGenerator();
 		new ClassGenerator();
+		new ProxyGenerator();
 	}
 }
--- src/generators/ProxyGenerator.java
+++ src/generators/ProxyGenerator.java
@@ -15,22 +15,16 @@ public class ProxyGenerator {
 	public ProxyGenerator() throws Exception {
 		ScanResult results = new FastClasspathScanner("objects").scan();
 		List<String> allResults = results.getNamesOfClassesWithAnnotation(Proxiable.class);
-		/*
+		String template = FileUtils.readFileToString(new File("template/ProxyMakerBase.txt"), Charset.defaultCharset());
+		StringBuilder methods = new StringBuilder();
 		for (String s : allResults) {
 			Class c = Class.forName(s);
-			String template = FileUtils.readFileToString(new File("template/ObjectProxyTemplate.txt"), Charset.defaultCharset());
-			StringBuilder getters = new StringBuilder();
-			StringBuilder setters = new StringBuilder();
-			for (Field f : c.getDeclaredFields()) {
-				String fN = Character.toUpperCase(f.getName().charAt(0)) + f.getName().substring(1);
-				getters.append("public " + f.getType().getSimpleName() + " get" + fN + "();\n");
-				setters.append("public void set" + fN + "(" + f.getType().getSimpleName() + " " + f.getName() + ");\n");
-			}
-			template = template.replaceAll("<OBJECT>", c.getSimpleName());
-			template = template.replaceAll("<GET_FIELDS>",  getters.toString());
-			template = template.replaceAll("<SET_FIELDS>", setters.toString());
-			FileUtils.write(new File("../Lab04Output/src/interfaces/" + c.getSimpleName() + "Proxy.java"), template, Charset.defaultCharset());
+			String templateE = FileUtils.readFileToString(new File("template/ProxyMakerForClasses.txt"), Charset.defaultCharset());
+			templateE = templateE.replaceAll("<CLASS_NAME>", c.getSimpleName());
+			methods.append(templateE);
+			methods.append("\n");
 		}
-		*/
+		template = template.replaceAll("<CREATE_PROXY>", methods.toString());
+		FileUtils.write(new File("../Lab04Output/src/proxy/ProxyMaker.java"), template, Charset.defaultCharset());
 	}
 }
